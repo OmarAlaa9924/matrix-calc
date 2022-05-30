d3ca236f926c19db1e62b84a9dc0f1bd3dc27dd5
OmarAlaa9924 <72947161+OmarAlaa9924@users.noreply.github.com>
2022-05-30 19:17:26 +0200

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -206,7 +206,7 @@ void cofactor(double matrix[50][50]){
     cofac[2][2] = (matrix[0][0]*matrix[1][1]) - (matrix[1][0]*matrix[0][1]); 
 }
 void inverse_matrix(double matrix[50][50],int r,int c){
-    double d;
+    
     square =true;
     large = false;
     if(r==c){
@@ -215,7 +215,7 @@ void inverse_matrix(double matrix[50][50],int r,int c){
         return;
     }
     
-     d=determinant(matrix,r);
+     double d=determinant(matrix,r);
         if(d != 0){
         d=1/d;
      if(r == 2){
